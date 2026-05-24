66939ba500b7108ad7df62a6f16fccc7df7e2ef1
vishnu_devs <vishnudevs9413@gmail.com>
2026-05-24 17:58:43 +0530

    security: make allowed CORS origins dynamic using FRONTEND_URL environment variable

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => array_values(array_unique(array_filter(array_merge(
+        ['http://localhost:5173', 'http://localhost:3000'],
+        explode(',', env('FRONTEND_URL', ''))
+    )))),
 
     'allowed_origins_patterns' => [],
 
